35fca470659e4b1ca98ee90f56d11fd4a22f5f01
Chris Pyles <chrispyles.99@gmail.com>
2018-09-18 20:29:06 -0700

    footer, js header and footer

---
+++ header.js
@@ -0,0 +1,12 @@
+var html = `<div class="fixed">
+
+			<h1 class="name"><a id="name" href="./index.html">Christopher Pyles</a></h1>
+
+				<p></p>
+
+				<p class="links"><a href="./index.html">About Me</a> | <a href="./education.html">Education</a> | <a href="./interests.html">Interests</a> | <a href="./achievements.html">Achievements & Co-Curriculars</a> | <a href="./experience.html">Experience</a> | <a href="./index.html#resume">Resume</a> | <a href="./jupyter_subpages/index.html" target="_blank">Jupyter</a></p>
+
+		</div>
+`;
+
+document.write(html);
