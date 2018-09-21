a9341612d87b15cf274750fd956a631cec94caa5
Chris Pyles <chrispyles.99@gmail.com>
2018-09-21 14:48:41 -0700

    updated class title

--- footer.js
+++ footer.js
@@ -5,7 +5,7 @@ var html = `<footer>
 		<tr>
 			<td><p class='footer'>Website designed by Christopher Pyles</p></td>
 			<td><p class='footer align-center'><a href='https://twitter.com/cp_berkeley' target='_blank'><img class='social' src='./assets/social/twitter.png'/></a><a href='https://linkedin.com/in/christopherapyles' target='_blank'><span style='margin-left: 5px;'><img src='./assets/social/linkedin.png' class='social'/></span></a><a href='https://github.com/chrispyles' target='_blank'><span style='margin-left: 5px;'><img src='./assets/social/github.png' class='social'/></span></a></p></td>
-			<td><p class='footer align-right'>Updated Sep 18, 2018</p></td>
+			<td><p class='footer align-right'>Updated Sep 21, 2018</p></td>
 		</tr>
 	</table>
 
