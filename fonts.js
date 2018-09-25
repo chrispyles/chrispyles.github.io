b5bbdde2b3e13cd1c03640846a88657e662cfc80
Chris Pyles <chrispyles.99@gmail.com>
2018-09-25 11:41:10 -0700

    moved fonts to js file

---
+++ fonts.js
@@ -0,0 +1,7 @@
+var html = `<link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Oswald:300|Roboto:300,300i,400,400i,500,500i,700,700i" rel="stylesheet"> -->
+		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,600,600i" rel="stylesheet">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400" rel="stylesheet"> -->
+		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">`
+
+document.write(html)
