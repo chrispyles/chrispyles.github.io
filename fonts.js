bb210ca70cce666211631a4e99e946f9ecdc89d4
Chris Pyles <chrispyles.99@gmail.com>
2018-09-26 10:03:44 -0700

    fixed fonts

--- fonts.js
+++ fonts.js
@@ -2,6 +2,6 @@ var html = `<link href="https://fonts.googleapis.com/css?family=Special+Elite" r
 		<!-- <link href="https://fonts.googleapis.com/css?family=Oswald:300|Roboto:300,300i,400,400i,500,500i,700,700i" rel="stylesheet"> -->
 		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,600,600i" rel="stylesheet">
 		<!-- <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400" rel="stylesheet"> -->
-		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">`
+		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">`;
 
-document.write(html)
+document.write(html);
