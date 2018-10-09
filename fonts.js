101d3bcde82876f03f055f2535b60b4c583ae084
Chris Pyles <chrispyles.99@gmail.com>
2018-10-09 12:22:03 -0700

    added recursive_hailstone

--- fonts.js
+++ fonts.js
@@ -1,7 +1,8 @@
-var html = `<link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet">
+var html = `<!-- <link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet"> -->
 		<!-- <link href="https://fonts.googleapis.com/css?family=Oswald:300|Roboto:300,300i,400,400i,500,500i,700,700i" rel="stylesheet"> -->
-		<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,600,600i" rel="stylesheet">
+		<!-- <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,300i,600,600i" rel="stylesheet"> -->
 		<!-- <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400" rel="stylesheet"> -->
-		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">`;
+		<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700&amp;amp;subset=cyrillic" rel="stylesheet">`;
 
 document.write(html);
