61da6c5684f9fc72c16130b82a96efb202ea1bf0
Chris Pyles <chrispyles.99@gmail.com>
2018-10-18 22:49:43 -0700

    fixed js links

--- header.js
+++ header.js
@@ -1,10 +1,10 @@
 var html = `<div class="fixed">
 
-			<h1 class="name"><a id="name" href="./index.html">Christopher Pyles</a></h1>
+			<h1 class="name"><a id="name" href="https://cpyles.com/index.html">Christopher Pyles</a></h1>
 
 				<p></p>
 
-				<p class="links"><a href="./index.html">About Me</a> | <a href="./education.html">Education</a> | <a href="./interests.html">Interests</a> | <a href="./achievements.html">Achievements & Co-Curriculars</a> | <a href="./experience.html">Experience</a> | <a href="./index.html#resume">Resume</a> | <a href="./jupyter_subpages/index.html" target="_blank">Jupyter</a></p>
+				<p class="links"><a href="https://cpyles.com/index.html">About Me</a> | <a href="https://cpyles.com/education.html">Education</a> | <a href="https://cpyles.com/interests.html">Interests</a> | <a href="https://cpyles.com/achievements.html">Achievements & Co-Curriculars</a> | <a href="https://cpyles.com/experience.html">Experience</a> | <a href="https://cpyles.com/index.html#resume">Resume</a> | <a href="https://cpyles.com/jupyter_subpages/index.html" target="_blank">Jupyter</a></p>
 
 		</div>
 `;
