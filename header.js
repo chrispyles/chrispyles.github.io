fc193ac5c2fe46abf8f0ff480dbbb01f87acd4c2
Chris Pyles <chrispyles.99@gmail.com>
2018-10-23 15:20:42 -0700

    fixed header

--- header.js
+++ header.js
@@ -4,7 +4,7 @@ var html = `<div class="fixed">
 
 				<p></p>
 
-				<p class="links"><a href="https://cpyles.com/index.html">About Me</a> | <a href="https://cpyles.com/education.html">Education</a> | <a href="https://cpyles.com/interests.html">Interests</a> | <a href="https://cpyles.com/achievements.html">Achievements & Co-Curriculars</a> | <a href="https://cpyles.com/experience.html">Experience</a> | <a href="https://cpyles.com/index.html#resume">Resume</a> | <a href="https://jupyter.cpyles.com" target="_blank">Jupyter</a></p>
+				<p class="links"><a href="./index.html">About Me</a> | <a href="./education.html">Education</a> | <a href="./interests.html">Interests</a> | <a href="./achievements.html">Achievements & Co-Curriculars</a> | <a href="./experience.html">Experience</a> | <a href="./index.html#resume">Resume</a> | <a href="https://jupyter.cpyles.com" target="_blank">Jupyter</a></p>
 
 		</div>
 `;
