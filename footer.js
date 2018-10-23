a4267321f6a7fdf9fe7699618c9f45c8076cc1a0
Chris Pyles <chrispyles.99@gmail.com>
2018-10-22 22:08:18 -0700

    fixed footer

--- footer.js
+++ footer.js
@@ -4,7 +4,18 @@ var html = `<footer>
 	<table class='footer'>
 		<tr>
 			<td><p class='footer'>Website designed by Christopher Pyles</p></td>
-			<td><p class='footer align-center'><a href='https://twitter.com/cp_berkeley' target='_blank'><img class='social' src='https://cpyles.com/assets/social/twitter.png'/></a><a href='https://linkedin.com/in/christopherapyles' target='_blank'><span style='margin-left: 5px;'><img src='https://cpyles.com/assets/social/linkedin.png' class='social'/></span></a><a href='https://github.com/chrispyles' target='_blank'><span style='margin-left: 5px;'><img src='https://cpyles.com/assets/social/github.png' class='social'/></span></a></p></td>
+			
+			<td><p class='footer align-center'><a href='https://linkedin.com/in/christopherapyles' target='_blank'>
+			<img src='https://cpyles.com/assets/social/linkedin.png' class='social'/></span></a>
+
+			<a href='https://github.com/chrispyles' target='_blank'><span style='margin-left: 5px;'>
+			<img src='https://cpyles.com/assets/social/github.png' class='social'/></a>
+
+			<a href='https://twitter.com/cp_berkeley' target='_blank'>
+			<span style='margin-left: 5px;'>
+			<img class='social' src='https://cpyles.com/assets/social/twitter.png'/></span></a>
+			
+			</p></td>
 			<td><p class='footer align-right'>Updated October 22, 2018</p></td>
 		</tr>
 	</table>
