00fdde796a4a486980f6a033835d45316170d585
Chris Pyles <chrispyles.99@gmail.com>
2019-01-15 08:30:38 -0800

    updated fonts.js and footer.js;

--- fonts.js
+++ fonts.js
@@ -1,4 +1,4 @@
-var html = `<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700&amp;amp;subset=cyrillic" rel="stylesheet">`;
+var html = `<!-- <link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700&amp;amp;subset=cyrillic" rel="stylesheet"> -->`;
 
 document.write(html);
